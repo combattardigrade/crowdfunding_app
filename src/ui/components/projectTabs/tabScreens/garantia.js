d7a9283bd97522ab349290c71d47744807dd9ea9
blitslabs <defi@blits.net>
2020-11-05 09:47:03 -0600

    garantia tab

--- src/ui/components/projectTabs/tabScreens/garantia.js
+++ src/ui/components/projectTabs/tabScreens/garantia.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   View,
   Image,
@@ -6,24 +6,42 @@ import {
 } from 'react-native';
 import Row from '../../row';
 import Line from '../../line';
-
 import styles from '../style';
-const descriptionTxt = 'test'
+import { connect } from 'react-redux';
+import { API } from '@env'
+
 const img =
   'https://image.freepik.com/free-photo/hand-pencil-pictures-students-writing-education-concept-with-copy-space_24883-1955.jpg';
-export default memo(() => {
-  return (
-    <View style={[styles.scene]}>
-      <Text style={[styles.heading, styles.bold]}>Garantía </Text>
-      <Text style={[styles.smTxt, { paddingVertical: 10 }]}>
-        {descriptionTxt}
-      </Text>
-      <Text style={[styles.smTxt, { paddingVertical: 10 }]}>
-        {descriptionTxt}
-      </Text>
-      <Line marginBottom={20} marginTop={10} />
 
-      <Image style={styles.fullImage} source={{ uri: img }} />
-    </View>
-  );
-});
+class Garantia extends Component {
+  render() {
+
+    const { projects, selectedProject } = this.props
+    const project = projects[selectedProject]
+    const imageURL = project.garantiaPictureId ? `${API}picture/${project.garantiaPictureId}` : img
+
+    return (
+      <View style={[styles.scene]}>
+        <Text style={[styles.heading, styles.bold]}>Garantía </Text>
+        <Text style={[styles.smTxt, { paddingVertical: 10 }]}>
+          Tipo de Garantía: {project?.garantia}
+        </Text>
+        <Text style={[styles.smTxt, { paddingVertical: 10 }]}>
+          {project?.detallesGarantia}
+        </Text>
+        <Line marginBottom={20} marginTop={10} />
+
+        <Image style={styles.fullImage} source={{ uri: imageURL }} />
+      </View>
+    );
+  }
+}
+
+function mapStateToProps({ projects, shared }){
+  return {
+    selectedProject: shared && shared.selectedProject,
+    projects
+  }
+}
+
+export default connect(mapStateToProps)(Garantia)
